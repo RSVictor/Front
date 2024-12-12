e47023a79e812c18aba529da8c83fdfc8ab15e80
RSVictor <victorribeiro304@hotmail.com>
2024-12-11 21:20:46 -0300

    update

--- library-api/server.js
+++ library-api/server.js
@@ -19,7 +19,7 @@ const upload = multer({ storage: storage }); // Corrigido para não usar .single
 
 const app = express();
 app.use(cors({
-    origin: 'https://front-iqbz.onrender.com',
+    origin: 'https://front-xi-sand.vercel.app/',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
 
